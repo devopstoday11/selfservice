725ef98ea0c212a7bd74ed3b5c7f19eb73a6428f
Omar Kohl <omar.kohl@lsexperts.de>
2015-01-07 16:38:34 +0100

    Fix package name in setup.py
    Change-Id: Ibca7a301f94a21c450b91c3fcdfa219dcf50f650

--- src/setup.py
+++ src/setup.py
@@ -6,7 +6,7 @@ except ImportError:
     from setuptools import setup, find_packages
 
 setup(
-    name='linotpselfservice',
+    name='LinOTPSelfservice',
     version='0.1',
     description='',
     author='',
